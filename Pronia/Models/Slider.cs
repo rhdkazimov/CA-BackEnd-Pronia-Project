f3a6866323219fd8f139bdcd56f10bb509d4e0d9
kazimov789 <118839541+kazimov789@users.noreply.github.com>
2023-06-02 18:57:20 +0400

    New Tables Add

---
+++ Pronia/Models/Slider.cs
@@ -0,0 +1,29 @@
+﻿using Pronia.Attiributes.ValidationAttiributes;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Pronia.Models
+{
+	public class Slider
+	{
+		public int Id { get; set; }
+		[MaxLength(20)]
+		public string Title { get; set; }
+		[Required]
+		[MaxLength(20)]
+		public string Name { get; set; }
+		[MaxLength(50)]
+		public string Desc { get; set; }
+		[MaxLength(15)]
+		public string BtnText { get; set; }
+		[MaxLength(150)]
+		public string BtnUrl { get; set; }
+		[Required]
+		[MaxLength(100)]
+		public string ImageName { get; set; }
+		[MaxFileSize(2097152)]
+		[AllowsFileType("image/jpeg", "image/png")]
+		[NotMapped]
+		public IFormFile ImageFile { get; set; }
+	}
+}
